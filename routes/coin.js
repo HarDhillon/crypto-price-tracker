c4d22f72f0f257b1c124b630104f07178af88e92
HarDhillon <har7173@gmail.com>
2024-04-29 08:56:30 +0100

    ensure user is authenticated before allowing them to use certain routes

--- routes/coin.js
+++ routes/coin.js
@@ -6,13 +6,13 @@ const coinController = require('../controllers/coin')
 
 router.get('/', ensureAuthenticated, coinController.getIndex)
 
-router.post('/add-coin', coinController.postCoin)
+router.post('/add-coin', ensureAuthenticated, coinController.postCoin)
 
-router.post('/buy-coin', coinController.postBuyCoin)
+router.post('/buy-coin', ensureAuthenticated, coinController.postBuyCoin)
 
 // TODO user router.delete when  we create front end OR use JS to post the form with DELETE
-router.post('/remove-buy', coinController.deleteBuyPrice)
+router.post('/remove-buy', ensureAuthenticated, coinController.deleteBuyPrice)
 
-router.post('/remove-coin-track', coinController.deleteCoinTrack)
+router.post('/remove-coin-track', ensureAuthenticated, coinController.deleteCoinTrack)
 
 module.exports = router
